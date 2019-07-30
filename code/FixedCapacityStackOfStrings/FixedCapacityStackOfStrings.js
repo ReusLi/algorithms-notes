408572ced4af763a9c57e388054876db076c126d
ReusLi <574399934@qq.com>
2019-07-30 22:51:47 +0800

    定容栈算法

--- code/FixedCapacityStackOfStrings/FixedCapacityStackOfStrings.js
+++ code/FixedCapacityStackOfStrings/FixedCapacityStackOfStrings.js
@@ -1,5 +1,23 @@
 /**
- * 定容栈
+ * 定容字符串栈
  */
 
-const 
+const Stack = require('../../data-structure/stack');
+
+const str = 'to be or not to - be - - that - - - is';
+
+const stack = new Stack();
+const run = (str) => {
+    let arr = str.split(' '),
+        i = 0;
+    while(i < arr.length) {
+        let chat = arr[i];
+        chat === '-'
+            ? console.log(stack.pop(chat))
+            : stack.push(chat)
+
+        i++;
+    }
+}
+
+run(str)
