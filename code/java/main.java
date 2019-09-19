1846f941f0c753fb1f153cc4c2cd6d4966004129
ReusLi <574399934@qq.com>
2019-09-19 22:06:24 +0800

    update code

--- code/java/main.java
+++ code/java/main.java
@@ -1,20 +1,19 @@
-public class main {
-    public static void main(String[] args) {
-        int[] arr = { 1, 9, 4, 2, 4, 7, 2, 3, 5, 2 };
+import base.base;;
 
+public class main {
+    public static void main (String[] args) {
+        Comparable[] arr = { 1, 9, 4, 2, 4, 7, 2, 3, 5, 2 };
         int temp;
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    temp = arr[i];
+                if (arr[i].compareTo(arr[j]) > 0){
+                    temp = (int)arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
                 }
             }
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
+        base.show(arr);
     }
 }
