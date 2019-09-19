3cf52eab62e33759ad3c703bd19a1cf0a819394a
ReusLi <574399934@qq.com>
2019-09-19 22:23:07 +0800

    insertion

--- code/java/main.java
+++ code/java/main.java
@@ -2,6 +2,28 @@ import base.base;;
 
 public class main {
     public static void main (String[] args) {
+        insetion();
+    }
+
+    /**
+     * 插入排序
+     */
+    public static void insetion() {
+        Comparable[] arr = {"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+        int N = arr.length;
+
+        for (int i = 1; i < N; i++) {
+            for (int j = i; j > 0 && base.less(arr[j], arr[j-1]); j--) {
+                base.exch(arr, j, j-1);
+            }
+        }
+        base.show(arr);
+    }
+
+    /**
+     * 快速排序
+     */
+    public static void quikeSort() {
         Comparable[] arr = { 1, 9, 4, 2, 4, 7, 2, 3, 5, 2 };
         int temp;
         for (int i = 0; i < arr.length - 1; i++) {
