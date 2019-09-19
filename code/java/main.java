6401d5f357fc80072826a58eabbbc5748f7d7cab
ReusLi <574399934@qq.com>
2019-09-19 22:29:56 +0800

    update code

--- code/java/main.java
+++ code/java/main.java
@@ -1,40 +1,40 @@
 import base.base;;
 
 public class main {
-    public static void main (String[] args) {
-        quikeSort();
-        // selection();
+    public static void main(String[] args) {
+        selection();
+        // insetion();
     }
 
     /**
      * 插入排序
      */
-    public static void selection() {
-        Comparable[] arr = {"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+    public static void insetion() {
+        Comparable[] arr = { "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E" };
         int N = arr.length;
 
         for (int i = 1; i < N; i++) {
-            for (int j = i; j > 0 && base.less(arr[j], arr[j-1]); j--) {
-                base.exch(arr, j, j-1);
+            for (int j = i; j > 0 && base.less(arr[j], arr[j - 1]); j--) {
+                base.exch(arr, j, j - 1);
             }
         }
         base.show(arr);
     }
 
     /**
-     * 快速排序
+     * 选择排序
      */
-    public static void quikeSort() {
+    public static void selection() {
         Comparable[] arr = { 1, 9, 4, 2, 4, 7, 2, 3, 5, 2 };
-        int temp;
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (base.less(arr[j], arr[i])){
-                    temp = (int)arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
+
+        int N = arr.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (base.less(arr[j], arr[min]))
+                    min = j;
             }
+            base.exch(arr, i, min);
         }
 
         base.show(arr);
